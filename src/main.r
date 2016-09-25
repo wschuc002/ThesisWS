2fdf658a22c6185ff54f200c74fdae70134244bb
wschuc002 <william.schuch@wur.nl>
2016-09-25 23:57:38 +0200

    Finds suitable office locations, based on commuting information and residential locations. Used OSM maps to generate commuting routes.

--- src/main.r
+++ src/main.r
@@ -1,4 +1,4 @@
-# Main script for determining Residence and Workplace from BAG data
+# Main script for determining Residence Workplace and commuting route from BAG data
 # Copyright (C) 2016 William Schuch
 #
 # This program is free software: you can redistribute it and/or modify
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## Check for required packages and install them (incl dependencies) if they are not installed yet.
-list.of.packages <- c("data.table","fossil", "geosphere", "sp", "rgdal", "rgeos", "maptools")
+list.of.packages <- c("data.table","sp","rgdal","foreign","rgeos","osrm")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -23,15 +23,9 @@ if(length(new.packages)) install.packages(new.packages)
 library(data.table)
 library(sp)
 library(rgdal)
-library(geosphere)
-library(fossil)
-
 library(foreign)
-
 library(rgeos)
-library(maptools)
-
-
+library(osrm)
 
 ## Clear the workspace
 #rm(list = ls()) 
@@ -39,37 +33,38 @@ library(maptools)
 getwd()
 
 zip_in = file.path("data", "bag-adressen-laatst.csv.zip")
-unzip(zip_in, exdir= file.path("data"))
 
 csv_in = file.path("data", "bagadres.csv")
-csv_out = file.path("output", "bagadres_ProvincieUtrecht.csv")
+if (exists(csv_in))
+  {
+    unzip(zip_in, exdir= file.path("data"))
+  }
 
-Gjson_out_name = "Residence_Workplace"
-Gjson_out_ext = ".geojson"
-Gjson_out = file.path("output", Gjson_out_name)
-Shape_out = file.path("output", "BAGadres_ProvincieUtrecht.shp")
+#csv_out = file.path("output", "bagadres_ProvincieUtrecht.csv")
 
 dbf_in = file.path("data", "verblijfsobjectgebruiksdoel.dbf")
 
 #require(data.table)
-BAG = fread(csv_in, sep=";", header=TRUE, integer64 = "numeric") # 64-bit only.  This will take about 5 minutes.
+BAG = fread(csv_in, sep=";", header=TRUE, integer64 = "numeric",
+            drop = c("huisletter", "object_type", "lon", "lat", "nevenadres")) # 64-bit only.  This will take about 5 minutes.
 #file.remove(csv_in)
 
 # Make a subset of BAG addresses
-BAG_sub = subset(BAG, provincie=="Utrecht")
+BAG_sub = subset(BAG, provincie=="Zuid-Holland")
 
 # Drop the useless attributes
-drops = c("gemeente","provincie","object_type","lon","lat","huisnummertoevoeging","nevenadres")
+drops = c("gemeente","provincie","huisnummertoevoeging") #,"lon","lat","nevenadres","object_type"
 SUB1 = BAG_sub[ , !(names(BAG_sub) %in% drops)]
 BAG_subbed = subset(BAG_sub, select = SUB1)
 
+#rm(BAG,BAG_sub)
 #BAG_subbed$object_id_C = as.character(BAG_subbed$object_id)
 
 # Read the database file with "Gebruiksdoelen" Goal of Use
 BAG_Doel = read.dbf(dbf_in) #as.is=FALSE       # this will take about 10 minutes.
 
 # Subset of 1st element and dbf element
-BAG_Doel = BAG_Doel[1]$dbf
+#BAG_Doel = BAG_Doel[1]$dbf
 
 #drops_dbf = c("AANDUIDING","AANDUID_01","BEGINDATUM","EINDDATUMT")
 keeps_dbf = c("IDENTIFICA","GEBRUIKSDO")
@@ -106,7 +101,7 @@ Residence_KEEPS = Residence@data$object_id %in% keeps_RS_Re
 Residence_subbed = subset(Residence, Residence_KEEPS)
 plot(Residence_subbed)
 
-# Subset: only office
+# Subset: only offices
 Workplace = subset(BAG_subbed_Doel_SPDF, BAG_subbed_Doel_SPDF@data$GEBRUIKSDO == "kantoorfunctie")
 
 # Pick 50 random offices object_id
@@ -116,20 +111,16 @@ keeps_RS_Of = RandObj_Of
 Workplace_KEEPS = Workplace@data$object_id %in% keeps_RS_Of
 Workplace_subbed = subset(Workplace, Workplace_KEEPS)
 
+# Remove duplicates in object_id
+Workplace_subbed = Workplace_subbed[!duplicated(Workplace_subbed@data$object_id),]
+
 # Visualisation & point highlighting
 plot(Workplace_subbed)
 points(Workplace_subbed[12,], col="red" )
 points(Residence_subbed[4,], col="blue" )
 
-# binden
-#Gebonden = rbind(Residence_subbed,Workplace_subbed)
-
-# Calculate distance by matrix
-
-#DIST_GEO = as.matrix(gDistance(Residence_subbed[1:20,],Workplace_subbed[1:20,]))
-
 # Create distance matrix
-DIST_GEO = matrix(data=NA, nrow=length(Residence_subbed), ncol=length(Workplace_subbed))
+DIST_GEO = matrix(data=NA, nrow=length(Residence_subbed), ncol=length(Workplace_subbed)) # empty/NA matrix
 for (z in seq(1, length(Residence_subbed), by=1))
   {
     for (w in seq(1, length(Workplace_subbed), by=1))
@@ -152,25 +143,94 @@ for (z in seq(1, length(Residence_subbed), by=1))
   {
     RV = sample(prob_CD, size=1)
     ResWor[z] = which.min(abs(RV-DIST_GEO[z,]))
-    
   }
 
 # Giving the pairs to the datasets
 Residence_subbed_WS = Residence_subbed
 Residence_subbed_WS@data$koppeling = ResWor
 
-# create DF and give koppeling#
-Workplace_subbed_DF = Workplace_subbed@data
-Workplace_subbed_DF$koppeling = seq(length(Workplace_subbed))
 
-# sort Residence by koppeling
-#Residence_subbed_sorted = Residence_subbed_WS[order(Residence_subbed_WS@data$koppeling),]
+
+## create DF and give koppeling#
+#Workplace_subbed_DF = Workplace_subbed@data
+#Workplace_subbed_DF$koppeling = seq(length(Workplace_subbed))
+
+# ALTERNATIVE for previous: DELETE Workplaces in SPDF that do not relate to "Residence_subbed_WS@data$koppeling"
+Workplace_subbed@data$NR = seq(length(Workplace_subbed))
+
+keeps_WP = Residence_subbed_WS@data$koppeling
+Workplace_KEEPS2 = Workplace_subbed@data$NR %in% keeps_WP
+Workplace_subbed_WS = subset(Workplace_subbed, Workplace_KEEPS2)
+
+
 
 # Merge two data frames by "koppeling"
 #ResWor_pairs = merge(Residence_subbed_sorted,Workplace_subbed_DF, by= "koppeling")
-ResWor_pairs = merge(Residence_subbed_WS,Workplace_subbed_DF, by= "koppeling")
+#ResWor_pairs = merge(Residence_subbed_WS,Workplace_subbed_DF, by= "koppeling")
 
 
 #Write to geojson
-writeOGR(ResWor_pairs, Gjson_out , Gjson_out_name, driver='GeoJSON')
+Gjson_out_name = "Residence"
+Gjson_out_ext = ".geojson"
+Gjson_out = file.path("output", Gjson_out_name)
+writeOGR(Residence_subbed_WS, Gjson_out , Gjson_out_name, driver='GeoJSON')
+file.rename(Gjson_out, file.path("output", paste(Gjson_out_name,Gjson_out_ext,sep="")))
+
+#Write to geojson
+Gjson_out_name = "Workplace"
+Gjson_out_ext = ".geojson"
+Gjson_out = file.path("output", Gjson_out_name)
+writeOGR(Workplace_subbed_WS, Gjson_out , Gjson_out_name, driver='GeoJSON')
+file.rename(Gjson_out, file.path("output", paste(Gjson_out_name,Gjson_out_ext,sep="")))
+
+
+# Transform RD new -> WGS84 for the route calculation (OSRM)
+WGS84 = "+init=epsg:4326"
+Residence_subbed_WS_WGS84 = Residence_subbed_WS
+Residence_subbed_WS_WGS84@proj4string = CRS(WGS84)
+Residence_subbed_WS_WGS84_T <- spTransform(Residence_subbed_WS, WGS84)
+
+Workplace_subbed_WS_WGS84 = Workplace_subbed_WS
+Workplace_subbed_WS_WGS84@proj4string = CRS(WGS84)
+Workplace_subbed_WS_WGS84_T <- spTransform(Workplace_subbed_WS, WGS84)
+
+# Put the coordinates in a dataframe
+Residence_subbed_WS_WGS84_T2 = Residence_subbed_WS_WGS84_T@data
+Residence_subbed_WS_WGS84_T2$lon1 = Residence_subbed_WS_WGS84_T@coords[,1]
+Residence_subbed_WS_WGS84_T2$lat1 = Residence_subbed_WS_WGS84_T@coords[,2]
+
+Workplace_subbed_WS_WGS84_T2 = Workplace_subbed_WS_WGS84_T@data
+Workplace_subbed_WS_WGS84_T2$lon2 = Workplace_subbed_WS_WGS84_T@coords[,1]
+Workplace_subbed_WS_WGS84_T2$lat2 = Workplace_subbed_WS_WGS84_T@coords[,2]
+
+# Merge Residence with Workplace
+RESWOR = merge(Residence_subbed_WS_WGS84_T2,Workplace_subbed_WS_WGS84_T2, by.x= "koppeling", by.y="NR")
+
+# Generate Commuting Routes (CR)
+CommutingRoutes = list()
+for (i in seq(1, length(Residence_subbed_WS_WGS84_T), by=1))
+  {
+    CommutingRoutes[i] = osrmRoute(src=c(paste("RES",i,sep="_"), RESWOR["lon1"][i,], RESWOR["lat1"][i,]),
+                           dst = c(paste("WOR",RESWOR["koppeling"][i,],sep="_"), RESWOR["lon2"][i,], RESWOR["lat2"][i,]),
+                             overview = "full", #"simplified"
+                             sp = TRUE)
+    #CommutingRoutes[[i]]@proj4string = CRS(RD_new)
+    CommutingRoutes[[i]] = spTransform(CommutingRoutes[[i]], RD_new)
+    CommutingRoutes[[i]]@lines[[1]]@ID = paste(CommutingRoutes[[i]]@lines[[1]]@ID,i)
+    
+    row.names(CommutingRoutes[[i]]) = as.character(i)
+}
+
+# Convert large list to a SpatialLinesDataFrame with all the Commuting Routes
+CommutingRoutes_SLDF = CommutingRoutes[[1]]
+for (i in seq(2, length(Residence_subbed_WS_WGS84_T), by=1))
+  {
+    CommutingRoutes_SLDF = rbind(CommutingRoutes_SLDF, CommutingRoutes[[i]])
+  }
+
+# Write to GeoJSON
+Gjson_out_name = "Commuting Routes"
+Gjson_out_ext = ".geojson"
+Gjson_out = file.path("output", Gjson_out_name)
+writeOGR(CommutingRoutes_SLDF, Gjson_out , Gjson_out_name, driver='GeoJSON')
 file.rename(Gjson_out, file.path("output", paste(Gjson_out_name,Gjson_out_ext,sep="")))
