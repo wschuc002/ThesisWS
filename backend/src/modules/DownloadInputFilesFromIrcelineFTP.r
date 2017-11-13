08f45cd5c9bc4ec6156b14a3f6270284f429e7eb
wschuc002 <william.schuch@wur.nl>
2017-11-13 13:10:23 +0100

    Minor changes

--- backend/src/modules/DownloadInputFilesFromIrcelineFTP.r
+++ backend/src/modules/DownloadInputFilesFromIrcelineFTP.r
@@ -23,27 +23,33 @@ if(length(new.packages)) install.packages(new.packages)
 ## Load the packages
 library(utils)
 
-DownloadInputFilesFromIrcelineFTP <- function(Filenames, ftp.pwd, out.dir, ...) # currently only working for pollutant data
+DownloadInputFilesFromIrcelineFTP <- function(Filenames, ftp.pwd, out.dir, pol, ...) # currently only working for pollutant data
 {
   print(paste0("Files to download:"))
   print(paste0(Filenames))
   
-  for (f in Filenames)
+  if (!dir.exists(out.dir)) { dir.create(out.dir) }
+  
+  outpol.dir = file.path(out.dir, toupper(pol))
+  if (!dir.exists(outpol.dir)) { dir.create(outpol.dir) }
+  
+  if (all(file.exists(file.path(Filenames))))
   {
-#     YearHourOfInterest = 25
-#     MonthDayHour = YearHourToMonthDayHour(YearHourOfInterest) #function returns "0102_1"
-#     #MonthDayHour = "0102_1"
-    
-  #   url.irceline = paste0("ftp://", ftp.pwd, "@ftp.irceline.be/rioifdm/2015_v421/",
-  #                         pol, "/", year.active, MonthDayHour, "_", toupper(pol), ".txt.bz2")
-    
-    url.irceline = paste0("ftp://", ftp.pwd, "@ftp.irceline.be/rioifdm/2015_v421/", pol, "/", f)
-    
-    #out.dir = file.path("..", "data", "IRCELINE_test")
-    if (!dir.exists(out.dir)) { dir.create(out.dir) }
-    
-    #download.file(url = url.irceline, destfile = file.path(out.dir, paste0(year.active, MonthDayHour, "_", toupper(pol), ".txt.bz2")))
-    download.file(url = url.irceline, destfile = file.path(out.dir, f))
-    
+    for (f in Filenames)
+    {
+      url.irceline = paste0("ftp://", ftp.pwd, "@ftp.irceline.be/rioifdm/2015_v421/", pol, "/", f)
+      
+      if (!file.exists(file.path(outpol.dir, f)))
+      {
+        #download.file(url = url.irceline, destfile = file.path(out.dir, paste0(year.active, MonthDayHour, "_", toupper(pol), ".txt.bz2")))
+        download.file(url = url.irceline, destfile = file.path(outpol.dir, f))
+      } else
+      {
+        print(paste0(file.path(outpol.dir, f), " already exists."))
+      }
+    }
+  } else
+  {
+    print(paste0("All the hi-res air quality files for ", toupper(pol), " already exist."))
   }
 }
